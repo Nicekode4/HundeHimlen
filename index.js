ac357b3a58721f2cb40b28261d83d0d9527e290c
Philip Thomsen <philip-th@live.dk>
2022-12-12 12:44:56 +0100

    Review setup

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import UserRouter from './routes/user.route.js'
+import InitRouter from "./routes/init.route.js";
 
 dotenv.config()
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(UserRouter)
+app.use(InitRouter)
 
 app.listen(port, () => {
     console.log(`Webserver running on http://localhost:${port}`);
